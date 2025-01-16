db413484594f9278539a5d7f01e5661d6e6632ab
Andras Lukacs <stratox777@gmail.com>
2025-01-16 10:18:42 +0100

    change cart option in header menu

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,13 +1,22 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
 import { MenubarModule } from 'primeng/menubar';
+import { RippleModule } from 'primeng/ripple';
+import { BadgeModule } from 'primeng/badge';
+import { OverlayBadgeModule } from 'primeng/overlaybadge';
+
+import { CartService } from '../../../cart/services/cart.service';
 
 @Component({
   selector: 'app-header',
-  imports: [MenubarModule],
+  imports: [BadgeModule, MenubarModule, RippleModule, OverlayBadgeModule, RouterLink],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
 })
 export class HeaderComponent {
+  constructor(readonly cartService: CartService) {}
+
   menuItems = [
     {
       label: 'Home',
@@ -17,9 +26,9 @@ export class HeaderComponent {
       label: 'Products',
       routerLink: '/products',
     },
-    {
+/*     {
       label: 'Cart',
       routerLink: '/cart',
-    },
+    }, */
   ];
 }
